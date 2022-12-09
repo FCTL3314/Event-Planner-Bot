87724ac26490eee58087c482a1c65250d4c06a71
Nikita <Solovev.nikita.05@gmail.com>
2022-12-09 23:49:46 +0300

    cancel command, cancel all states

--- handlers/commands/cancel_command.py
+++ handlers/commands/cancel_command.py
@@ -15,4 +15,7 @@ def register_cancel_command_handler(dp: aiogram.Dispatcher):
                                 commands=['cancel'])
     dp.register_message_handler(callback=send_cancel_translating_message, commands=['cancel'],
                                 state=[states.create_event_states.CreateEventStates.get_event_name,
-                                       states.create_event_states.CreateEventStates.get_event_description])
+                                       states.create_event_states.CreateEventStates.get_event_description,
+                                       states.create_event_states.CreateEventStates.get_event_picture,
+                                       states.create_event_states.CreateEventStates.select_channel,
+                                       states.create_event_states.CreateEventStates.is_all_correct])
