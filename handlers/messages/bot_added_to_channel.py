182570307fc53d74b637752008045c7a93e85170
Nikita <Solovev.nikita.05@gmail.com>
2022-12-09 12:10:05 +0300

    message text improvements

--- handlers/messages/bot_added_to_channel.py
+++ handlers/messages/bot_added_to_channel.py
@@ -13,23 +13,23 @@ async def bot_added_to_channel(message: aiogram.types.Message):
             with utils.database.database as db:
                 db.add_channel(channel_name=channel_info['title'], channel_id=channel_info['id'])
             for admin in BOT_ADMIN_IDS:
-                await bot.send_message(chat_id=admin, text=f'Бот добавлен в канал {channel_info["title"]}.')
+                await bot.send_message(chat_id=admin, text=f'✅Бот добавлен в канал: [{channel_info["title"]}].')
         elif status == 'kicked' or status == 'left':
             with utils.database.database as db:
                 db.remove_channel(channel_id=channel_info['id'])
             for admin in BOT_ADMIN_IDS:
-                await bot.send_message(chat_id=admin, text=f'Бот удалён из канала {channel_info["title"]}.')
+                await bot.send_message(chat_id=admin, text=f'✅Бот удалён из канала: [{channel_info["title"]}].')
     elif channel_info['type'] == 'group':
         if status == 'member':
             with utils.database.database as db:
                 db.add_group(group_name=channel_info['title'], group_id=channel_info['id'])
             for admin in BOT_ADMIN_IDS:
-                await bot.send_message(chat_id=admin, text=f'Бот добавлен в группу {channel_info["title"]}.')
+                await bot.send_message(chat_id=admin, text=f'✅Бот добавлен в группу: [{channel_info["title"]}].')
         elif status == 'kicked' or status == 'left':
             with utils.database.database as db:
                 db.remove_group(group_id=channel_info['id'])
             for admin in BOT_ADMIN_IDS:
-                await bot.send_message(chat_id=admin, text=f'Бот удалён из группы {channel_info["title"]}.')
+                await bot.send_message(chat_id=admin, text=f'✅Бот удалён из группы: [{channel_info["title"]}].')
 
 
 def register_bot_added_to_channel_handler(dp: aiogram.Dispatcher):
