d155b1c6640ae509dfc1d8c5ee73190a26a811d7
Nikita <Solovev.nikita.05@gmail.com>
2022-12-09 23:48:11 +0300

    edit command prompt text

--- utils/on_startup.py
+++ utils/on_startup.py
@@ -7,7 +7,7 @@ from loader import bot
 async def on_startup(dp: aiogram.Dispatcher):
     await bot.set_my_commands([
         aiogram.types.BotCommand('start', 'Начальная информация.'),
-        aiogram.types.BotCommand('create_event', 'Создать мероприятие / событие.'),
+        aiogram.types.BotCommand('create_event', 'Создать событие.'),
     ])
     with utils.database.database as db:
         db.create_tables()
