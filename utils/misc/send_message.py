31076fec6fccde74be6cad81845b07e398016c94
Nikita <Solovev.nikita.05@gmail.com>
2022-12-10 00:04:08 +0300

    decomposition, create new function that send preview post message

--- utils/misc/send_message.py
+++ utils/misc/send_message.py
@@ -1,5 +1,17 @@
 import aiogram
+import keyboards
 
 
 async def send_unable_execute_stop_command_message(message: aiogram.types.Message):
     await message.answer(text="❕Вы не создаёте событие.")
+
+
+async def send_preview_of_event_post(message: aiogram.types.Message, event_picture_id, event_name, event_description):
+    if event_picture_id:
+        await message.answer_photo(photo=event_picture_id, caption=f"*{event_name}*\n{event_description}",
+                                   reply_markup=keyboards.inline.send_event.send_event_keyboard(),
+                                   parse_mode='Markdown')
+    else:
+        await message.answer(text=f'*{event_name}*\n{event_description}',
+                             reply_markup=keyboards.inline.send_event.send_event_keyboard(),
+                             parse_mode='Markdown')
