ede24ddd2a75bc9c50c254e977a610639cc25fcd
Nikita <Solovev.nikita.05@gmail.com>
2022-12-09 23:48:51 +0300

    function decomposition (create_channel_and_group_strings into create_channels_text and get_channels_indexes)

--- utils/misc/misc.py
+++ utils/misc/misc.py
@@ -1,14 +1,20 @@
 from typing import List
 
 
-async def create_channel_and_group_strings(channels: List[tuple], groups: List[tuple]):
+async def create_channels_text(channels: List[tuple], groups: List[tuple]):
     result = ''
-    channels_indexes = dict()
     for i, channel in enumerate(channels, 1):
         result += f'{i}. {channel[1]}\n'
-        channels_indexes[str(i)] = channel[0]
     for i, group in enumerate(groups, len(channels) + 1):
         result += f'{i}. {group[1]}\n'
+    return result
+
+
+async def get_channels_indexes(channels: List[tuple], groups: List[tuple]):
+    channels_indexes = dict()
+    for i, channel in enumerate(channels, 1):
+        channels_indexes[str(i)] = channel[0]
+    for i, group in enumerate(groups, len(channels) + 1):
         channels_indexes[str(i)] = group[0]
-    return result, channels_indexes
+    return channels_indexes
 
