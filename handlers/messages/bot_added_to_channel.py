88839a85dd75635697ce3eb5a8df2a2bb727cc08
Nikita <Solovev.nikita.05@gmail.com>
2022-12-09 12:06:33 +0300

    kicked or left check, save group / channel to database

--- handlers/messages/bot_added_to_channel.py
+++ handlers/messages/bot_added_to_channel.py
@@ -1,4 +1,5 @@
 import aiogram
+import utils
 
 from data.config import BOT_ADMIN_IDS
 from loader import bot
@@ -9,16 +10,24 @@ async def bot_added_to_channel(message: aiogram.types.Message):
     status = message["new_chat_member"]["status"]
     if channel_info['type'] == 'channel':
         if status == 'administrator':
+            with utils.database.database as db:
+                db.add_channel(channel_name=channel_info['title'], channel_id=channel_info['id'])
             for admin in BOT_ADMIN_IDS:
                 await bot.send_message(chat_id=admin, text=f'Бот добавлен в канал {channel_info["title"]}.')
-        elif status == 'kicked':
+        elif status == 'kicked' or status == 'left':
+            with utils.database.database as db:
+                db.remove_channel(channel_id=channel_info['id'])
             for admin in BOT_ADMIN_IDS:
                 await bot.send_message(chat_id=admin, text=f'Бот удалён из канала {channel_info["title"]}.')
     elif channel_info['type'] == 'group':
         if status == 'member':
+            with utils.database.database as db:
+                db.add_group(group_name=channel_info['title'], group_id=channel_info['id'])
             for admin in BOT_ADMIN_IDS:
                 await bot.send_message(chat_id=admin, text=f'Бот добавлен в группу {channel_info["title"]}.')
-        elif status == 'left':
+        elif status == 'kicked' or status == 'left':
+            with utils.database.database as db:
+                db.remove_group(group_id=channel_info['id'])
             for admin in BOT_ADMIN_IDS:
                 await bot.send_message(chat_id=admin, text=f'Бот удалён из группы {channel_info["title"]}.')
 
