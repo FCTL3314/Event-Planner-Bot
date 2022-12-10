e42133695e32af8577e0b2ba07a6a5ee2412dd11
Nikita <Solovev.nikita.05@gmail.com>
2022-12-10 14:10:55 +0300

    delete start command

--- utils/on_startup.py
+++ utils/on_startup.py
@@ -6,7 +6,6 @@ from loader import bot
 
 async def on_startup(dp: aiogram.Dispatcher):
     await bot.set_my_commands([
-        aiogram.types.BotCommand('start', 'Начальная информация.'),
         aiogram.types.BotCommand('create_event', 'Создать событие.'),
     ])
     with utils.database.database as db:
