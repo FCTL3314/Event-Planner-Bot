e7d6ea264bf879ed4d6422f197e4edf3d4562794
Nikita <Solovev.nikita.05@gmail.com>
2022-12-10 22:37:49 +0300

    readability

--- main.py
+++ main.py
@@ -7,9 +7,12 @@ import utils
 from loader import dp
 from data.config import LOGGING_LEVEL, LOGGING_FILE_NAME, LOGGING_FILE_MODE, LOGGING_FORMAT
 
-# logging.basicConfig(level=LOGGING_LEVEL, filename=f"{LOGGING_FILE_NAME}.log", filemode=LOGGING_FILE_MODE,
-#                     format=LOGGING_FORMAT)
 
+logging.basicConfig(level=LOGGING_LEVEL, filename=f"{LOGGING_FILE_NAME}.log", filemode=LOGGING_FILE_MODE,
+                    format=LOGGING_FORMAT)
+
+handlers.callbacks.without_picture.register_without_picture(dp=dp)
+handlers.callbacks.cancel_event_send.register_cancel_event_send_handlers(dp=dp)
 handlers.messages.bot_channel_actions.register_bot_added_to_channel_handlers(dp=dp)
 handlers.commands.cancel_command.register_cancel_command_handlers(dp=dp)
 handlers.commands.create_event_command.register_create_event_command_handlers(dp=dp)
