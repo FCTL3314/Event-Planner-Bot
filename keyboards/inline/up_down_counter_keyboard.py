fd4b684b2e1d71e7b9f78c2b00be215f69513b2f
Nikita <Solovev.nikita.05@gmail.com>
2022-12-10 13:26:27 +0300

    delete cause not use

--- keyboards/inline/up_down_counter_keyboard.py
+++
@@ -1,9 +0,0 @@
-import aiogram
-
-
-def up_down_counter_keyboard():
-    inline_keyboard = aiogram.types.InlineKeyboardMarkup()
-    inline_keyboard_plus_button = aiogram.types.InlineKeyboardButton(text='👍🏼 0', callback_data='up')
-    inline_keyboard_minus_button = aiogram.types.InlineKeyboardButton(text='👎🏼 0', callback_data='down')
-    inline_keyboard.row(inline_keyboard_plus_button, inline_keyboard_minus_button)
-    return inline_keyboard
