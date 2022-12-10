dec4ef507cf2e19e3e3f9bd8eb692476091c1f2b
Nikita <Solovev.nikita.05@gmail.com>
2022-12-11 00:10:20 +0300

    send preview of survey function

--- utils/misc/send_message.py
+++ utils/misc/send_message.py
@@ -3,10 +3,10 @@ import keyboards
 
 
 async def send_unable_execute_stop_command_message(message: aiogram.types.Message):
-    await message.answer(text="❕Вы не создаёте событие, что бы отменить его создание.")
+    await message.answer(text="❕Вы не создаёте событие / опрос, что бы отменить его создание.")
 
 
-async def send_preview_of_event_post(message: aiogram.types.Message, event_picture_id, event_name, event_description):
+async def send_preview_of_event(message: aiogram.types.Message, event_picture_id, event_name, event_description):
     if event_picture_id:
         await message.answer_photo(photo=event_picture_id, caption=f"*{event_name}*\n{event_description}",
                                    reply_markup=keyboards.inline.send_event.send_event_keyboard(),
@@ -15,3 +15,8 @@ async def send_preview_of_event_post(message: aiogram.types.Message, event_pictu
         await message.answer(text=f'*{event_name}*\n{event_description}',
                              reply_markup=keyboards.inline.send_event.send_event_keyboard(),
                              parse_mode='Markdown')
+
+
+async def send_preview_of_survey(message: aiogram.types.Message, survey_name, survey_answers):
+    await message.answer_poll(question=survey_name, options=survey_answers.split('@'), is_anonymous=False,
+                              reply_markup=keyboards.inline.send_survey.send_survey_keyboard())
