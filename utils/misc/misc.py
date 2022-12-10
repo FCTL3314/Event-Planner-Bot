029533407e5bc1227bc9ebe19bfbca268a348791
Nikita <Solovev.nikita.05@gmail.com>
2022-12-10 22:08:23 +0300

    create channels text and indexes even if channels or groups is True

--- utils/misc/misc.py
+++ utils/misc/misc.py
@@ -3,17 +3,21 @@ from typing import List
 
 async def create_channels_text(channels: List[tuple], groups: List[tuple]):
     result = ''
-    for i, channel in enumerate(channels, 1):
-        result += f'{i}. {channel[1]}\n'
-    for i, group in enumerate(groups, len(channels) + 1):
-        result += f'{i}. {group[1]}\n'
+    if channels:
+        for i, channel in enumerate(channels, 1):
+            result += f'{i}. {channel[1]}\n'
+    if groups:
+        for i, group in enumerate(groups, len(channels) + 1):
+            result += f'{i}. {group[1]}\n'
     return result
 
 
 async def get_channels_indexes(channels: List[tuple], groups: List[tuple]):
     channels_indexes = dict()
-    for i, channel in enumerate(channels, 1):
-        channels_indexes[str(i)] = channel[0]
-    for i, group in enumerate(groups, len(channels) + 1):
-        channels_indexes[str(i)] = group[0]
+    if channels:
+        for i, channel in enumerate(channels, 1):
+            channels_indexes[str(i)] = channel[0]
+    if groups:
+        for i, group in enumerate(groups, len(channels) + 1):
+            channels_indexes[str(i)] = group[0]
     return channels_indexes
