56f1c2044c8421597ac2b02a5fac5f2279fe3ac7
Nikita <Solovev.nikita.05@gmail.com>
2022-12-11 00:09:24 +0300

    now function cancel all states with cancel button push, readability

---
+++ handlers/callbacks/cancel_buttons.py
@@ -0,0 +1,16 @@
+import aiogram
+import states
+
+from loader import bot
+
+
+async def cancel_buttons(callback: aiogram.types.CallbackQuery, state: aiogram.dispatcher.FSMContext):
+    await bot.send_message(chat_id=callback.from_user.id, text='ℹ️*Отправка события / опроса отменена.*',
+                           parse_mode='Markdown')
+    await state.finish()
+
+
+def register_cancel_buttons_handlers(dp: aiogram.Dispatcher):
+    dp.register_callback_query_handler(callback=cancel_buttons, text='cancel',
+                                       state=[states.create_event_states.CreateEventStates.send_event,
+                                              states.create_survey_states.CreateSurveyStates.send_survey])
