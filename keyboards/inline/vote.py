7165db912ed61b754fb5b9efd2f2e58652e756ec
Nikita <Solovev.nikita.05@gmail.com>
2022-12-12 18:21:53 +0300

    readability

--- keyboards/inline/vote.py
+++ keyboards/inline/vote.py
@@ -4,7 +4,9 @@ import aiogram
 def vote_keyboard(amount_of_likes: int, amount_of_dislikes: int, vote_limit: int):
     inline_keyboard = aiogram.types.InlineKeyboardMarkup()
     like_button = aiogram.types.InlineKeyboardButton(text=f'👍🏼 {amount_of_likes}',
-                                                     callback_data=f'like {amount_of_likes} {amount_of_dislikes} {vote_limit}')
+                                                     callback_data=f'like {amount_of_likes} {amount_of_dislikes} '
+                                                                   f'{vote_limit}')
     dislike_button = aiogram.types.InlineKeyboardButton(text=f'👎🏼 {amount_of_dislikes}',
-                                                        callback_data=f'dislike {amount_of_likes} {amount_of_dislikes} {vote_limit}')
+                                                        callback_data=f'dislike {amount_of_likes} {amount_of_dislikes} '
+                                                                      f'{vote_limit}')
     return inline_keyboard.row(like_button, dislike_button)
