c24a2290cc7f8979eb578eeb398a93443d76a215
Nikita <Solovev.nikita.05@gmail.com>
2022-12-13 11:49:09 +0300

    add statistics command

--- handlers/commands/statistics_commdnd.py
+++ handlers/commands/statistics_commdnd.py
@@ -1,8 +1,12 @@
 import aiogram
+import utils
+
+from loader import bot
 
 
 async def statistics_command(message: aiogram.types.Message):
-    pass
+    with utils.database.database as db:
+        print(db.execute(query=f'SELECT DISTINCT (message_id) FROM event_votes'))
 
 
 def register_statistics_command(dp: aiogram.Dispatcher):
