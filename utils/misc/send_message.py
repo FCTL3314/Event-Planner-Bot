824829224eb1071d8f19f6c2086ce9136c68a8b1
Nikita <Solovev.nikita.05@gmail.com>
2022-12-14 22:19:59 +0300

    message text edit

--- utils/misc/send_message.py
+++ utils/misc/send_message.py
@@ -3,7 +3,7 @@ import keyboards
 
 
 async def send_unable_execute_stop_command_message(message: aiogram.types.Message):
-    await message.answer(text="❕Вы не создаёте событие / опрос, что бы отменить его создание.")
+    await message.answer(text="❕Вам нечего отменять.")
 
 
 async def send_preview_of_event(message: aiogram.types.Message, event_picture_id, event_name, event_description):
