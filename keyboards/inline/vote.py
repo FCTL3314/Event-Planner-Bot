bfce2ac8a2604a0c30e545a666bb4fdd7e81e92d
Nikita <Solovev.nikita.05@gmail.com>
2022-12-14 11:27:06 +0300

    edit vote keyboard

--- keyboards/inline/vote.py
+++ keyboards/inline/vote.py
@@ -1,12 +1,18 @@
 import aiogram
 
 
-def vote_keyboard(amount_of_likes: int, amount_of_dislikes: int, vote_limit: int):
+def vote_keyboard(likes_count: int, record_count: int, think_count: int, link_button_url: str = None,
+                  link_button_name: str = None):
     inline_keyboard = aiogram.types.InlineKeyboardMarkup()
-    like_button = aiogram.types.InlineKeyboardButton(text=f'👍🏼 {amount_of_likes}',
-                                                     callback_data=f'like {amount_of_likes} {amount_of_dislikes} '
-                                                                   f'{vote_limit}')
-    dislike_button = aiogram.types.InlineKeyboardButton(text=f'👎🏼 {amount_of_dislikes}',
-                                                        callback_data=f'dislike {amount_of_likes} {amount_of_dislikes} '
-                                                                      f'{vote_limit}')
-    return inline_keyboard.row(like_button, dislike_button)
+    like_button = aiogram.types.InlineKeyboardButton(text=f'👍🏼 Приду {likes_count}',
+                                                     callback_data=f'like')
+    record_button = aiogram.types.InlineKeyboardButton(text=f'💫 Запись {record_count}',
+                                                       callback_data=f'record')
+    think_button = aiogram.types.InlineKeyboardButton(text=f'💤 Думаю {think_count}',
+                                                      callback_data=f'think')
+    inline_keyboard.row(like_button, record_button, think_button)
+    if link_button_url and link_button_name:
+        link_button = aiogram.types.InlineKeyboardButton(text=f'{link_button_name}', url=f'{link_button_url}',
+                                                         callback_data=f'think')
+        inline_keyboard.add(link_button)
+    return inline_keyboard
