7af9a55e562702088ad61b9768838c32f0a5746c
Nikita <Solovev.nikita.05@gmail.com>
2022-12-14 11:49:02 +0300

    add more states to cancel command

--- handlers/commands/cancel_command.py
+++ handlers/commands/cancel_command.py
@@ -20,7 +20,8 @@ def register_cancel_command_handlers(dp: aiogram.Dispatcher):
                                        states.create_event_states.CreateEventStates.get_event_picture,
                                        states.create_event_states.CreateEventStates.get_event_description,
                                        states.create_event_states.CreateEventStates.get_vote_limit,
-                                       states.create_event_states.CreateEventStates.get_link_button_name_and_url,
+                                       states.create_event_states.CreateEventStates.get_link_button_name,
+                                       states.create_event_states.CreateEventStates.get_link_button_url,
                                        states.create_event_states.CreateEventStates.get_channels_to_send,
                                        states.create_event_states.CreateEventStates.send_event,
                                        states.statistic_command_states.CreateStatisticsStates.get_channels])
