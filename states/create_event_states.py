680789fd00057d7386ba76596a5bb5e3ab8de5ba
Nikita <Solovev.nikita.05@gmail.com>
2022-12-14 11:49:19 +0300

    state decomposition

--- states/create_event_states.py
+++ states/create_event_states.py
@@ -6,6 +6,7 @@ class CreateEventStates(StatesGroup):
     get_event_picture = State()
     get_event_description = State()
     get_vote_limit = State()
-    get_link_button_name_and_url = State()
+    get_link_button_name = State()
+    get_link_button_url = State()
     get_channels_to_send = State()
     send_event = State()
