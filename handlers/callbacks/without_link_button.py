ace9cc422078625b272c24d12407c05410ebdc59
Nikita <Solovev.nikita.05@gmail.com>
2022-12-14 11:49:26 +0300

    state decomposition

--- handlers/callbacks/without_link_button.py
+++ handlers/callbacks/without_link_button.py
@@ -12,9 +12,9 @@ async def without_picture(callback: aiogram.types.CallbackQuery, state: aiogram.
     await bot.send_message(chat_id=callback.message.chat.id,
                            text=f'❕*Отправьте номер канала, либо укажите через пробел номера каналов, '
                                 f'в которые необходимо отправить событие:*\n{channels_text}', parse_mode='Markdown')
-    await states.create_event_states.CreateEventStates.next()
+    await states.create_event_states.CreateEventStates.get_channels_to_send.set()
 
 
 def register_without_link_button(dp: aiogram.Dispatcher):
     dp.register_callback_query_handler(callback=without_picture, text='without_link_button',
-                                       state=states.create_event_states.CreateEventStates.get_link_button_name_and_url)
+                                       state=states.create_event_states.CreateEventStates.get_link_button_name)
