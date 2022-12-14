be1cf2645b36de0900ab3c53909da0dade6a3be4
Nikita <Solovev.nikita.05@gmail.com>
2022-12-14 11:24:37 +0300

    add all states to cancel command & edit message text

--- handlers/commands/cancel_command.py
+++ handlers/commands/cancel_command.py
@@ -7,7 +7,7 @@ from data.config import BOT_ADMIN_IDS
 
 async def cancel_command(message: aiogram.types.Message, state: aiogram.dispatcher.FSMContext):
     if message.from_user.id in BOT_ADMIN_IDS:
-        await message.answer(text='ℹ️*Создание события / опроса остановлено.*', parse_mode='Markdown',
+        await message.answer(text='ℹ️*Создание события остановлено.*', parse_mode='Markdown',
                              reply_markup=aiogram.types.reply_keyboard.ReplyKeyboardRemove())
         await state.finish()
 
@@ -20,6 +20,7 @@ def register_cancel_command_handlers(dp: aiogram.Dispatcher):
                                        states.create_event_states.CreateEventStates.get_event_picture,
                                        states.create_event_states.CreateEventStates.get_event_description,
                                        states.create_event_states.CreateEventStates.get_vote_limit,
+                                       states.create_event_states.CreateEventStates.get_link_button_name_and_url,
                                        states.create_event_states.CreateEventStates.get_channels_to_send,
                                        states.create_event_states.CreateEventStates.send_event,
                                        states.statistic_command_states.CreateStatisticsStates.get_channels])
