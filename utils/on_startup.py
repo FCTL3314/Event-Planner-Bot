be8c2ae87dd712fa52b9c512d638244cdaa76996
Nikita <Solovev.nikita.05@gmail.com>
2022-12-15 12:04:07 +0300

    start command

--- utils/on_startup.py
+++ utils/on_startup.py
@@ -6,6 +6,7 @@ from loader import bot
 
 async def on_startup(dp: aiogram.Dispatcher):
     await bot.set_my_commands([
+        aiogram.types.BotCommand('start', 'Начальная информация.'),
         aiogram.types.BotCommand('create_event', 'Создать событие.'),
         aiogram.types.BotCommand('statistics', 'Посмотреть статистику событий.'),
         aiogram.types.BotCommand('cancel', 'Отменить создание события, опроса.'),
