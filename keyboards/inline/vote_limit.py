417127ff65cfc81aa1943d89a4475e7fedc9628f
Nikita <Solovev.nikita.05@gmail.com>
2022-12-16 12:01:02 +0300

    keyboard text edit

--- keyboards/inline/vote_limit.py
+++ keyboards/inline/vote_limit.py
@@ -3,8 +3,7 @@ import aiogram
 
 def vote_limit_keyboard(limit: int, link_button_name=None, link_button_url=None) -> aiogram.types.InlineKeyboardMarkup:
     inline_keyboard = aiogram.types.InlineKeyboardMarkup()
-    limit_filled_button = aiogram.types.InlineKeyboardButton(text=f'❌{limit}/{limit} лимит исчерпан',
-                                                             callback_data='limit')
+    limit_filled_button = aiogram.types.InlineKeyboardButton(text=f'❌{limit}/{limit}❌', callback_data='limit')
     inline_keyboard.row(limit_filled_button)
     if link_button_name:
         inline_keyboard.add(aiogram.types.InlineKeyboardButton(text=link_button_name, url=link_button_url,
