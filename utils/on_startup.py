01ad987d2f0184d7cea8150db42079429a3194fb
Nikita <Solovev.nikita.05@gmail.com>
2022-12-16 12:12:28 +0300

    command text edit

--- utils/on_startup.py
+++ utils/on_startup.py
@@ -9,7 +9,7 @@ async def on_startup(dp: aiogram.Dispatcher):
         aiogram.types.BotCommand('start', 'Начальная информация.'),
         aiogram.types.BotCommand('create_event', 'Создать событие.'),
         aiogram.types.BotCommand('statistics', 'Посмотреть статистику событий.'),
-        aiogram.types.BotCommand('cancel', 'Отменить создание события, опроса.'),
+        aiogram.types.BotCommand('cancel', 'Отмена всех действий.'),
         aiogram.types.BotCommand('clear', 'Удалить все мероприятия.'),
     ])
     with utils.database.database as db:
