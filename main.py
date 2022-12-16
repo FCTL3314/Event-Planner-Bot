90d2464bedf55f24a2bc0ed73da2cb1655196ba4
Nikita <Solovev.nikita.05@gmail.com>
2022-12-16 12:03:08 +0300

    register clear_command handler

--- main.py
+++ main.py
@@ -8,16 +8,17 @@ from loader import dp
 from data.config import LOGGING_LEVEL, LOGGING_FILE_NAME, LOGGING_FILE_MODE, LOGGING_FORMAT
 
 
-logging.basicConfig(level=LOGGING_LEVEL, filename=f"{LOGGING_FILE_NAME}.log", filemode=LOGGING_FILE_MODE,
-                    format=LOGGING_FORMAT)
+# logging.basicConfig(level=LOGGING_LEVEL, filename=f"{LOGGING_FILE_NAME}.log", filemode=LOGGING_FILE_MODE,
+#                     format=LOGGING_FORMAT)
 
+handlers.commands.cancel_command.register_cancel_command_handlers(dp=dp)
+handlers.commands.clear_command.register_clear_command_handler(dp=dp)
 handlers.commands.start_command.register_start_command_handler(dp=dp)
 handlers.callbacks.vote_buttons.register_vote_buttons_handler(dp=dp)
 handlers.callbacks.without_picture.register_without_picture(dp=dp)
 handlers.callbacks.without_link_button.register_without_link_button(dp=dp)
 handlers.callbacks.cancel_buttons.register_cancel_buttons_handlers(dp=dp)
 handlers.messages.bot_channel_actions.register_bot_added_to_channel_handlers(dp=dp)
-handlers.commands.cancel_command.register_cancel_command_handlers(dp=dp)
 handlers.commands.create_event_command.register_create_event_command_handlers(dp=dp)
 handlers.commands.statistics_command.register_statistics_command(dp=dp)
 
