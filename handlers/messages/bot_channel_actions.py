874aa28789636919b8844f248b23986aae47a9bd
Nikita <Solovev.nikita.05@gmail.com>
2022-12-18 22:06:09 +0300

    fix bug when bot doesn't react to adding to the group

--- handlers/messages/bot_channel_actions.py
+++ handlers/messages/bot_channel_actions.py
@@ -6,29 +6,28 @@ from loader import bot
 
 
 async def bot_channel_actions(message: aiogram.types.Message):
-    if message["from"]["id"] in BOT_ADMIN_IDS:
-        channel_info = message.chat
-        status = message["new_chat_member"]["status"]
-        if channel_info['type'] == 'channel':
-            if status == 'administrator':
-                with utils.database.database as db:
-                    db.execute(f"INSERT INTO channels VALUES ({channel_info['id']}, '{channel_info['title']}')")
-                for admin in BOT_ADMIN_IDS:
-                    await bot.send_message(chat_id=admin, text=f'ℹ️Бот добавлен в канал: {channel_info["title"]}.')
-            elif status == 'kicked' or status == 'left':
-                await utils.misc.delete_all_chat_info(chat_id=channel_info['id'])
-                for admin in BOT_ADMIN_IDS:
-                    await bot.send_message(chat_id=admin, text=f'ℹ️Бот удалён из канала: {channel_info["title"]}.')
-        elif channel_info['type'] == 'group' or channel_info['type'] == 'supergroup':
-            if status == 'member':
-                with utils.database.database as db:
-                    db.execute(f"INSERT INTO groups VALUES ({channel_info['id']}, '{channel_info['title']}')")
-                for admin in BOT_ADMIN_IDS:
-                    await bot.send_message(chat_id=admin, text=f'ℹ️Бот добавлен в группу: {channel_info["title"]}.')
-            elif status == 'kicked' or status == 'left':
-                await utils.misc.delete_all_chat_info(chat_id=channel_info['id'])
-                for admin in BOT_ADMIN_IDS:
-                    await bot.send_message(chat_id=admin, text=f'ℹ️Бот удалён из группы: {channel_info["title"]}.')
+    channel_info = message.chat
+    status = message["new_chat_member"]["status"]
+    if channel_info['type'] == 'channel':
+        if status == 'administrator':
+            with utils.database.database as db:
+                db.execute(f"INSERT INTO channels VALUES ({channel_info['id']}, '{channel_info['title']}')")
+            for admin in BOT_ADMIN_IDS:
+                await bot.send_message(chat_id=admin, text=f'ℹ️Бот добавлен в канал: {channel_info["title"]}.')
+        elif status == 'kicked' or status == 'left':
+            await utils.misc.delete_all_chat_info(chat_id=channel_info['id'])
+            for admin in BOT_ADMIN_IDS:
+                await bot.send_message(chat_id=admin, text=f'ℹ️Бот удалён из канала: {channel_info["title"]}.')
+    elif channel_info['type'] == 'group' or channel_info['type'] == 'supergroup':
+        if status == 'member':
+            with utils.database.database as db:
+                db.execute(f"INSERT INTO groups VALUES ({channel_info['id']}, '{channel_info['title']}')")
+            for admin in BOT_ADMIN_IDS:
+                await bot.send_message(chat_id=admin, text=f'ℹ️Бот добавлен в группу: {channel_info["title"]}.')
+        elif status == 'kicked' or status == 'left':
+            await utils.misc.delete_all_chat_info(chat_id=channel_info['id'])
+            for admin in BOT_ADMIN_IDS:
+                await bot.send_message(chat_id=admin, text=f'ℹ️Бот удалён из группы: {channel_info["title"]}.')
 
 
 def register_bot_added_to_channel_handlers(dp: aiogram.Dispatcher):
